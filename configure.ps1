c201b8c0d57251b0e42db1dc866807567371ef32
rubyj24 <114274153+rubyj24@users.noreply.github.com>
2024-09-19 23:33:50 -0400

    Update configure.ps1

--- configure.ps1
+++ configure.ps1
@@ -22,10 +22,7 @@ Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 Invoke-Webrequest -uri "go.microsoft.com/fwlink/p/?LinkID=2182910&clcid=0x409&culture=en-us&country=us" -outfile onedrive.exe
 
 # uhh not sure
-cd C:\Users\runneradmin\Downloads 
-
-# fuck you
-./onedrive.exe
+$command = “C:\Users\runneradmin\Downloads\onedrive.exe”
 
 # Change password to the one we set-up as RDP_PASSWORD on our repo settings.
 Set-LocalUser -Name "runneradmin" -Password (ConvertTo-SecureString -AsPlainText "P@ssw0rd!" -Force)
