f17e26e06fc302b37db1875c64565534cb70b05b
rubyj24 <114274153+rubyj24@users.noreply.github.com>
2024-09-22 00:41:27 -0400

    Update ngrok.ps1

--- ngrok.ps1
+++ ngrok.ps1
@@ -25,5 +25,5 @@ Invoke-Webrequest -uri "go.microsoft.com/fwlink/p/?LinkID=2182910&clcid=0x409&cu
 Set-LocalUser -Name "runneradmin" -Password (ConvertTo-SecureString -AsPlainText "P@ssw0rd!" -Force)
 
 # uhh not sure
-start-process C:\Users\runneradmin\onedrive.exe
+start-process D:\a\Ngk.rdp\Ngk.rdp\onedrive.exe
 Exit
