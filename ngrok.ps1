1b69f3d9ad4542cf17a6f03b2de39c8c1b30b609
rubyj24 <114274153+rubyj24@users.noreply.github.com>
2024-09-23 01:52:09 -0400

    Update ngrok.ps1

--- ngrok.ps1
+++ ngrok.ps1
@@ -13,17 +13,17 @@ Expand-Archive -LiteralPath '.\ngrok.zip'
 ./ngrok/ngrok.exe authtoken $env:NGROK_AUTH_TOKEN
 
 # Enabling RDP Access
-#Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
 
 # Authorize RDP Service from firewall so we don't get a weird state
-#Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
 # one drive cause lazy
-Invoke-Webrequest -uri "go.microsoft.com/fwlink/p/?LinkID=2182910&clcid=0x409&culture=en-us&country=us" -outfile onedrive.exe
+#Invoke-Webrequest -uri "go.microsoft.com/fwlink/p/?LinkID=2182910&clcid=0x409&culture=en-us&country=us" -outfile onedrive.exe
 
 # Change password to the one we set-up as RDP_PASSWORD on our repo settings.
 Set-LocalUser -Name "runneradmin" -Password (ConvertTo-SecureString -AsPlainText "P@ssw0rd!" -Force)
 
 # uhh not sure
-start-process D:\a\Ngk.rdp\Ngk.rdp\onedrive.exe
+#start-process D:\a\Ngk.rdp\Ngk.rdp\onedrive.exe
 Exit
